d66da54c84a4555205839ffccc44ed77b553d254
Marcos Vinicius <marcosvtenoriodev@gmail.com>
2022-09-18 07:53:27 -0300

    Funcionalidade de cálculo e resultado adicionada

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,3 +15,14 @@ function back()
     var result = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = result.substring(0, result.length -1);
 }
+
+function calc()
+{
+    var result = document.getElementById('result').innerHTML;
+    if (result)
+
+    {
+    document.getElementById('result').innerHTML = eval(result);
+    }
+    
+}
